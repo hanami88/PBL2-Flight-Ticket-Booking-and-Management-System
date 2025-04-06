1f2def1ced322a2ff5778c5a4708bb980650a318
Luan31-dev <kieutran.142a@gmail.com>
2025-04-06 22:04:40 +0700

    Initial Commit

---
+++ PBL2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PBL2
+//
+//  Created by cc on 6/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
